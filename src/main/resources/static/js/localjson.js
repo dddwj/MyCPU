ba5746bc87b0d8f62ad466cb81e3416af3ca09a5
dwj <dwj2930@163.com>
2019-07-03 14:50:41 +0800

    完成前台布局

--- src/main/resources/static/js/localjson.js
+++ src/main/resources/static/js/localjson.js
@@ -223,6 +223,96 @@ var back_front = {
     "10": {
         "blockup": [
             {
+                "PID": 13,
+                "name": "helloworld.java",
+                "prio": null,
+                "round": null,
+                "arrivalTime": null,
+                "serviceTime": 60,
+                "cpuTime": 15,
+                "remainNeedTime": 30,
+                "count": null,
+                "state": "BLOCK_UP"
+            },
+            {
+                "PID": 5443,
+                "name": "IDEA CLion.app",
+                "prio": null,
+                "round": null,
+                "arrivalTime": null,
+                "serviceTime": 80,
+                "cpuTime": 14,
+                "remainNeedTime": 45,
+                "count": null,
+                "state": "BLOCK_UP"
+            }
+        ],
+        "ready": [
+            {
+                "PID": 1,
+                "name": "ls -la",
+                "prio": null,
+                "round": null,
+                "arrivalTime": null,
+                "serviceTime": 30,
+                "cpuTime": 12,
+                "remainNeedTime": null,
+                "count": null,
+                "state": "READY"
+            },
+            {
+                "PID": 2,
+                "name": "top",
+                "prio": null,
+                "round": null,
+                "arrivalTime": null,
+                "serviceTime": 40,
+                "cpuTime": 36,
+                "remainNeedTime": null,
+                "count": null,
+                "state": "READY"
+            },
+            {
+                "PID": 3,
+                "name": "python",
+                "prio": null,
+                "round": null,
+                "arrivalTime": null,
+                "serviceTime": 27,
+                "cpuTime": 18,
+                "remainNeedTime": null,
+                "count": null,
+                "state": "READY"
+            },
+            {
+                "PID": 13,
+                "name": "cnpm run dev",
+                "prio": 32,
+                "round": null,
+                "arrivalTime": 2,
+                "serviceTime": 50,
+                "cpuTime": 10,
+                "remainNeedTime": 40,
+                "count": null,
+                "state": "RUN"
+            }
+        ],
+        "run": [
+            {
+                "PID": 11,
+                "name": "java -jar",
+                "prio": 3,
+                "round": null,
+                "arrivalTime": 2,
+                "serviceTime": 50,
+                "cpuTime": 10,
+                "remainNeedTime": 40,
+                "count": null,
+                "state": "RUN"
+            }
+        ],
+        "finish": [
+            {
                 "PID": 0,
                 "name": "hello",
                 "prio": null,
@@ -232,7 +322,9 @@ var back_front = {
                 "cpuTime": null,
                 "remainNeedTime": null,
                 "count": null,
-                "state": "BLOCK_UP"
+                "state": "FINISH",
+                "turnoverTime": 50,
+                "weightedTurnoverTime": 16
             },
             {
                 "PID": 0,
@@ -244,7 +336,9 @@ var back_front = {
                 "cpuTime": null,
                 "remainNeedTime": null,
                 "count": null,
-                "state": "BLOCK_UP"
+                "state": "FINISH",
+                "turnoverTime": 35,
+                "weightedTurnoverTime": 25
             },
             {
                 "PID": 0,
@@ -255,6 +349,36 @@ var back_front = {
                 "serviceTime": null,
                 "cpuTime": null,
                 "remainNeedTime": null,
+                "count": null,
+                "state": "FINISH",
+                "turnoverTime": 43,
+                "weightedTurnoverTime": 12
+            }
+        ]
+    },
+    "11": {
+        "blockup": [
+            {
+                "PID": 13,
+                "name": "helloworld.java",
+                "prio": null,
+                "round": null,
+                "arrivalTime": null,
+                "serviceTime": 60,
+                "cpuTime": 15,
+                "remainNeedTime": 30,
+                "count": null,
+                "state": "BLOCK_UP"
+            },
+            {
+                "PID": 5443,
+                "name": "IDEA CLion.app",
+                "prio": null,
+                "round": null,
+                "arrivalTime": null,
+                "serviceTime": 80,
+                "cpuTime": 14,
+                "remainNeedTime": 45,
                 "count": null,
                 "state": "BLOCK_UP"
             }
@@ -299,9 +423,9 @@ var back_front = {
         ],
         "run": [
             {
-                "PID": 11,
-                "name": "java -jar",
-                "prio": 3,
+                "PID": 13,
+                "name": "cnpm run dev",
+                "prio": 32,
                 "round": null,
                 "arrivalTime": 2,
                 "serviceTime": 50,
@@ -313,20 +437,20 @@ var back_front = {
         ],
         "finish": [
             {
-                "PID": 0,
-                "name": "hello",
-                "prio": null,
+                "PID": 11,
+                "name": "java -jar",
+                "prio": 3,
                 "round": null,
-                "arrivalTime": null,
-                "serviceTime": null,
-                "cpuTime": null,
-                "remainNeedTime": null,
+                "arrivalTime": 2,
+                "serviceTime": 50,
+                "cpuTime": 10,
+                "remainNeedTime": 40,
                 "count": null,
-                "state": "FINISH"
+                "state": "RUN"
             },
             {
-                "PID": 0,
-                "name": "hello",
+                "PID": 155,
+                "name": "IDEA.app",
                 "prio": null,
                 "round": null,
                 "arrivalTime": null,
@@ -334,11 +458,13 @@ var back_front = {
                 "cpuTime": null,
                 "remainNeedTime": null,
                 "count": null,
-                "state": "FINISH"
+                "state": "FINISH",
+                "turnoverTime": 50,
+                "weightedTurnoverTime": 16
             },
             {
-                "PID": 0,
-                "name": "hello",
+                "PID": 564,
+                "name": "QQ.app",
                 "prio": null,
                 "round": null,
                 "arrivalTime": null,
@@ -346,7 +472,23 @@ var back_front = {
                 "cpuTime": null,
                 "remainNeedTime": null,
                 "count": null,
-                "state": "FINISH"
+                "state": "FINISH",
+                "turnoverTime": 35,
+                "weightedTurnoverTime": 25
+            },
+            {
+                "PID": 345,
+                "name": "WeChat.app",
+                "prio": null,
+                "round": null,
+                "arrivalTime": null,
+                "serviceTime": null,
+                "cpuTime": null,
+                "remainNeedTime": null,
+                "count": null,
+                "state": "FINISH",
+                "turnoverTime": 43,
+                "weightedTurnoverTime": 12
             }
         ]
     }
